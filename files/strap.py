0bb7f67d39681b7e0d5c612ca950404cc9258c51
Bill Sideris <bill88t@feline.gr>
2024-02-27 22:48:23 +0200

    Initial add

---
+++ files/strap.py
@@ -0,0 +1,8 @@
+shutil.copy("nano.lja", path.join(root, "bin", "nano.lja"))
+
+try:
+    mkdir(path.join(root, "bin/nano"))
+except FileExistsError:
+    pass
+for i in ["init.py", "main.py"]:
+    shutil.copy(i, path.join(root, "bin/nano", i))
---
+++ files/uninstaller.py
@@ -0,0 +1,4 @@
+be.based.run("rm /bin/nano.lja /bin/nano/init.py /bin/nano/main.py")
+be.based.run("rmdir /bin/nano")
+
+be.api.setvar("return", "0")
