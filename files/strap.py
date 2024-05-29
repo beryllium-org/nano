614d6cccd093a05990eb7126a9aa612afd9198e4
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:09:06 +0300

    copyfile

--- files/strap.py
+++ files/strap.py
@@ -1,8 +1,8 @@
-shutil.copy("nano.lja", path.join(root, "bin", "nano.lja"))
+shutil.copyfile("nano.lja", path.join(root, "bin", "nano.lja"))
 
 try:
     mkdir(path.join(root, "bin/nano"))
 except FileExistsError:
     pass
 for i in ["init.py", "main.py", "loop.py"]:
-    shutil.copy(i, path.join(root, "bin/nano", i))
+    shutil.copyfile(i, path.join(root, "bin/nano", i))
